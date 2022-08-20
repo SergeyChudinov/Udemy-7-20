e332a32460b195ffea777ebae3a6e10a05b52afd
SergeyChudinov <sergeychudinov1986@gmail.com>
2022-08-20 22:44:02 +0500

    Make lesson-7-20

---
+++ my-app/src/AppHeader.js
@@ -0,0 +1,20 @@
+import { NavLink} from 'react-router-dom'; 
+import './appHeader.css';
+
+const AppHeader = () => {
+    return (
+        <header className="app__header">
+            <nav className="app__menu">
+                <ul>
+                    <li><NavLink style={({ isActive }) => ({color: isActive ? 'white' : 'inherit' })}
+                        end  to='/'>Characters</NavLink></li>
+                    /
+                    <li><NavLink style={({ isActive }) => ({color: isActive ? 'white' : 'inherit' })}
+                        to='/yup'>Yup</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default AppHeader;
